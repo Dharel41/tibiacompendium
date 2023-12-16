21b9951f7f9840e6fc8057852c3227e7499d753d
Dharel41 <45524611+Dharel41@users.noreply.github.com>
2023-12-16 03:13:27 +0100

    basic get API for hunting places

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 from tibiacompendium.city.views import city
 from tibiacompendium.character.views import character
+from tibiacompendium.hunt.views import hunt
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 app.register_blueprint(city)
 app.register_blueprint(character)
+app.register_blueprint(hunt)
 
 
 @app.route('/')
