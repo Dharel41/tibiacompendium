e5f433f66a643a3bc36d329b0badda746e552f1a
Dharel41 <45524611+Dharel41@users.noreply.github.com>
2023-12-18 01:32:42 +0100

    hunting place POST method

--- hunt/views.py
+++ hunt/views.py
@@ -1,14 +1,37 @@
-from flask import Blueprint, jsonify, request, abort, render_template, url_for, redirect
-from tibiacompendium.db import mysql
+from flask import Blueprint, request, render_template, url_for, redirect, abort
 from pymysql.err import IntegrityError, DataError
+from tibiacompendium.db import mysql
+from tibiacompendium.hunt.helpers import count_exp_speed
+from tibiacompendium.hunt.validators import validate_hunting_place_data
 
 # API using database cursor connection
 hunt = Blueprint('hunt', __name__)
 @hunt.route('/hunting-places', methods=['POST'])
 def add_huntingplace():
-    print("TODO ADD LOGIC")
-    print(request.form)
-    return redirect(url_for('hunt.render_huntingplaces_template', hunt_type=request.form.get('hunt_type')))
+    errors, validated_data = validate_hunting_place_data(request.form)
+
+    if errors:
+        return render_template('huntingplace.html', errors=errors)
+    else:
+        exp_on_hour = count_exp_speed(validated_data.get('exp_on_hour'), validated_data.get('exp_factor'))
+        with mysql.cursor() as cursor:
+            try:
+                cursor.execute(
+                    """
+                        INSERT INTO huntingplace 
+                            (name, character_levels, hunt_type, exp_on_hour, gold_on_hour, 
+                            rune, equipment_with_resistance, description)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                    """, 
+                    (validated_data.get('name'), validated_data.get('character_levels'), validated_data.get('hunt_type'), 
+                     exp_on_hour, validated_data.get('gold_on_hour'),validated_data.get('rune'), 
+                     validated_data.get('eq_with_resistance'), validated_data.get('description')
+                    ))
+                mysql.commit()
+            except (IntegrityError, DataError) as e:
+                abort(400, str(e))
+            
+            return redirect(url_for('hunt.render_huntingplaces_template', hunt_type=validated_data.get('hunt_type')))
 
 @hunt.route('/hunting-places', methods=['GET'])
 def render_huntingplaces_template():
@@ -25,6 +48,6 @@ def render_huntingplaces_template():
                 COALESCE(gold_on_hour, ''), COALESCE(rune, ''), COALESCE(equipment_with_resistance, ''), COALESCE(description, '')
             FROM 
                 huntingplace
-            """ + where_condition)
+            """ + where_condition + "order by exp_on_hour desc")
         data = cursor.fetchall()
         return render_template('huntingplace.html', hunting_places=data, hunt_type=hunt_type)
